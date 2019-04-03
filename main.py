196ebf19c56c71cdc7722c8d61d67545661288cf
rthornton3i <43607226+rthornton3i@users.noreply.github.com>
2019-04-03 16:55:23 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ class Suduko:
     def __init__(self,frames):
         self.frames = frames
     
-    def defaultOpts(self):
+    def singleOpts(self):
         optFrames = []
         for n in range(9):
             opts = []
             for row in range(9):
                 for colm in range(9):
                     frameInd = math.floor(row/3)*3 + math.floor(colm/3)
-                    el = self.grid[row,colm]
+                    
+                    el = self.rows[row][colm]
                     
                     if frameInd == n:
                         if el != 0:
@@ -21,50 +22,169 @@ class Suduko:
                         else:
                             r = self.rows[row]
                             c = self.colms[colm]
-                            b = self.frames[frameInd]
+                            f = self.frames[frameInd]
                         
-                            opt = [m for m in range(1,10) if m not in r and m not in c and m not in b]
+                            opt = [m for m in range(1,10) if m not in r and m not in c and m not in f]
                             opts.append(opt)
             
             optFrames.append(opts)
         
         self.optFrames = optFrames
         
-    def commonOpts(self):
-        [self.gridOpts,self.rowOpts,self.colmOpts] = gridMat(self.optFrames)
+    def hidSingleOpts(self):
+        [_,_,rowOpts,colmOpts] = gridMat(frames=self.optFrames)
         
-        
-        gridOpts = []
+        optFrames = []
         for n in range(9):
-            frameOpts = []
-            ind = 0
+            opts = []
             for row in range(9):
                 for colm in range(9):
                     frameInd = math.floor(row/3)*3 + math.floor(colm/3)
-                    if frameInd == n:
-                        frameOpts.append(opts[ind])
-                        
-                    ind += 1
+                    optsFrame = [opt for elOpt in self.optFrames[frameInd] for opt in elOpt]
+                    optsRow = [opt for elOpt in rowOpts[row] for opt in elOpt]
+                    optsColm = [opt for elOpt in colmOpts[colm] for opt in elOpt]
                     
-            gridOpts.append(frameOpts)
-        
-        for frame in gridOpts:
-            frameOpts = [opt for el in frame for opt in el]
+                    elOpts = rowOpts[row][colm]
+                    
+                    if frameInd == n:
+                        if len(elOpts) == 1:
+                            opts.append(elOpts)
+                        else:
+                            uniqueCheck = False
+                            for m in elOpts:
+                                if optsFrame.count(m) == 1 or optsRow.count(m) == 1 or optsColm.count(m) == 1:
+                                    uniqueCheck = True
+                                    break
+                            
+                            if uniqueCheck:
+                                opts.append([m])
+                            else:
+                                opts.append(elOpts)
             
-            for el in frame:
-                try:            
-                    if len(el) == 1:
-                        self.grid[row,colm] = self.opts[ind][0]
-                        defCheck = True
-                        
-                        print((row,colm,self.grid[row,colm]))
-                except:
-                    pass
-#                if frameOpts.count(n) = 1
+            optFrames.append(opts)
             
-        self.opts = gridOpts
+        self.optFrames = optFrames
     
-    def frameMat(self):
+    def solve(self):
+        def singleSolve():
+            while True:
+                tempGrid = np.copy(self.grid)
+                
+                self.singleOpts()
+                [_,_,rowOpts,_] = gridMat(frames=self.optFrames)
+                
+                for row in range(9):
+                    for colm in range(9):
+                        if len(rowOpts[row][colm]) == 1:
+                            self.grid[row,colm] = rowOpts[row][colm][0]
+    #                        print((row,colm,rowOpts[row][colm][0]))
+                            
+                [self.frames,_,self.rows,self.colms] = gridMat(grid=self.grid)
+                        
+                if np.sum(tempGrid - self.grid) == 0:
+                    break      
+        
+        def hidSingleSolve():
+            while True:
+                tempGrid = np.copy(self.grid)
+                
+                self.hidSingleOpts()
+                [_,_,rowOpts,_] = gridMat(frames=self.optFrames)
+                
+                for row in range(9):
+                    for colm in range(9):
+                        if len(rowOpts[row][colm]) == 1:
+                            self.grid[row,colm] = rowOpts[row][colm][0]
+    #                        print((row,colm,rowOpts[row][colm][0]))
+                            
+                [self.frames,_,self.rows,self.colms] = gridMat(grid=self.grid)
+                        
+                if np.sum(tempGrid - self.grid) == 0:
+                    break
+                
+        while True:
+            tempGrid = np.copy(self.grid)
+            
+            singleSolve()
+            hidSingleSolve()
+            
+            if np.sum(tempGrid - self.grid) == 0:
+                break        
+    
+    def guess(self):
+        grid = np.copy(self.grid)
+        [_,_,rowOpts,_] = gridMat(frames=self.optFrames)
+        
+        n = 1
+        while True:
+            if n % 1000 == 0:
+                print('Loop: ' + str(n))
+            
+            for row in range(9):
+                for colm in range(9):
+                    if grid[row,colm] == 0:
+                        grid[row,colm] = rowOpts[row][colm][np.random.randint(len(rowOpts[row][colm]))]
+            
+            [self.frames,_,self.rows,self.colms] = gridMat(grid=grid)
+            self.check()
+            
+            if self.gridCheck is False:
+                grid = np.copy(self.grid)
+            else:
+                break
+            
+            n += 1
+            
+    def check(self):
+        gridCheck = True
+        if gridCheck:
+            for frame in self.frames:
+                for n in range(1,10):
+                    if frame.count(n) != 1:
+                        gridCheck = False
+                        break
+                
+                if gridCheck is False:
+                    break
+        
+        if gridCheck:
+            for row in self.rows:
+                for n in range(1,10):
+                    if row.count(n) != 1:
+                        gridCheck = False
+                        break
+                
+                if gridCheck is False:
+                    break
+        
+        if gridCheck:
+            for colm in self.colms:
+                for n in range(1,10):
+                    if colm.count(n) != 1:
+                        gridCheck = False
+                        break
+                
+                if gridCheck is False:
+                    break
+            
+        self.gridCheck = gridCheck
+    
+    def run(self):
+        [_,self.grid,self.rows,self.colms] = gridMat(frames=self.frames)
+        inGrid = np.copy(self.grid)
+       
+        self.solve()
+        self.check()
+        
+        if self.gridCheck is False:
+            self.guess()
+        
+        return [inGrid,self.grid,self.optFrames]
+
+#==============================================================================
+
+def gridMat(frames=None,grid=None):
+    def frameMat():
         frames = []
         for n in range(9):
             frame = []
@@ -72,55 +192,12 @@ class Suduko:
                 for colm in range(9):
                     frameInd = math.floor(row/3)*3 + math.floor(colm/3)
                     if frameInd == n:
-                        frame.append(self.grid[row,colm])
+                        frame.append(grid[row,colm])
             
             frames.append(frame)
-                        
-        self.frames = frames
-    
-    def solve(self):
-        defCheck = False
-        while True:
-            self.optsMat()
-            
-            ind = 0
-            for row in range(9):
-                for colm in range(9):
-                    try:            
-                        if len(self.opts[ind]) == 1:
-                            self.grid[row,colm] = self.opts[ind][0]
-                            defCheck = True
-                            
-                            print((row,colm,self.grid[row,colm]))
-                    except:
-                        pass
-                    
-                    ind += 1
-                    
-            self.frameMat()
-            
-            self.rowMat()
-            self.colmMat()
-            
-            self.gridMat()
-
-            if not defCheck:
-                break
-    
-    def run(self):
-        [self.grid,self.rows,self.colms] = gridMat(self.frames)
-        inGrid = self.grid
         
-        self.defaultOpts()
-        [self.gridOpts,self.rowOpts,self.colmOpts] = gridMat(self.optFrames)
+        return frames
         
-#        self.solve()
-#        self.optsMat()
-        
-        return [self.gridOpts,self.rowOpts,self.colmOpts]
-
-
-def gridMat(frames):
     def rowMat():
         rows = []
         for n in range(3):
@@ -149,17 +226,38 @@ def gridMat(frames):
                 
         return colms
     
+    def gridSet():
+        grid = np.zeros((9,9))
+        rowInd = 0
+        for row in rows:
+            colmInd = 0
+            for colm in row:
+                if type(colm) is int:
+                    grid[rowInd,colmInd] = colm
+                elif type(colm) is list:
+                    grid[rowInd,colmInd] = colm[0] if len(colm) == 1 else 0
+                    
+                colmInd += 1
+                    
+            rowInd += 1
+                         
+        return grid
+    
+    if frames is None:
+        frames = frameMat()
+        
     rows = rowMat()
     colms = colmMat()
     
-    grid = np.array(colms).transpose()
+    grid = gridSet()
     
-    return [grid,rows,colms]
+    return [frames,grid,rows,colms]
+
+#==============================================================================
 
 frames = [[3,0,4,1,8,0,0,0,0],[0,7,0,0,6,0,1,3,0],[0,0,9,0,3,7,8,0,0],
           [8,3,0,5,0,0,0,0,0],[0,0,0,0,2,8,0,4,0],[7,0,5,0,9,0,0,0,3],
           [0,0,0,0,0,0,0,0,0],[0,5,0,0,9,0,6,0,0],[9,7,6,0,5,0,0,0,0]]
 
 suduko = Suduko(frames)
-[grid,row,colm] = suduko.run()
-    
+[inGrid,outGrid,optFrames] = suduko.run()    
